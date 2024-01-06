899ca383c206467f05d6badbcac7f485fcc6fb5b
AleksaGrocic <agrocic@gmail.com>
2024-01-06 16:59:58 +0100

    Fixed icon

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.whenReady().then(() => {
     width: 750,
     height: 850,
     frame: false,
-    icon: path.join(__dirname, 'src/icon.ico'),
+    icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
